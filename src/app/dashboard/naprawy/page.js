85a9be2b5dd46b5719bf7d1bd41d09dbade09582
Mikolaj Pazdzior <mikolajpaz@gmail.com>
2025-01-02 22:59:08 +0100

    Niepełna funkcjonalność panelu właściciela/naprawy

--- src/app/dashboard/naprawy/page.js
+++ src/app/dashboard/naprawy/page.js
@@ -1,12 +1,16 @@
 'use client'
 
-import React from "react";
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, getKeyValue, Spinner, Link } 
+import React, { useState } from "react";
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, Spinner, useDisclosure, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Form, Input, Textarea } 
 from "@nextui-org/react";
 import { useAsyncList } from "@react-stately/data";
+import Image from "next/image";
 
 export default function Naprawy() {
     const [isLoading, setIsLoading] = React.useState(true);
+    const [filterValue, setFilterValue] = React.useState("");
+
+    const hasSearchFilter = Boolean(filterValue);
 
     let list = useAsyncList({
         async load({ signal }) {
@@ -38,6 +42,63 @@ export default function Naprawy() {
         },
     });
 
+    const items = React.useMemo(() => {
+        let filteredRepairs = [...list.items];
+
+        if (hasSearchFilter) {
+            filteredRepairs = filteredRepairs.filter((repair) => 
+                (
+                    repair.naprawaid + repair.stan + repair.data_rozpoczecia + repair.data_zakonczenia + repair.protokol_naprawy + repair.opis_usterki
+                ).toLowerCase().includes(filterValue.toLowerCase())
+            );
+        }
+
+        return filteredRepairs;
+    }, [list.items, filterValue]);
+
+    const onSearchChange = React.useCallback((value) => {
+        if (value) {
+            setFilterValue(value);
+        } 
+        else {
+            setFilterValue("");
+        }
+        list.load;
+    }, []);
+
+    const onClear = React.useCallback(() => {
+        setFilterValue("");
+        list.load;
+    }, []);
+
+    const editModal = useDisclosure();
+    const newModal = useDisclosure();
+    const [obj, setObj] = useState(null);
+    const [isNew, setNew] = useState(false);
+    const [modalHeader, setModalHeader] = useState("");
+
+    const addItem = () => {
+        setModalHeader("Nowe zgłoszenie");
+        setNew(true);
+        newModal.onOpen();
+    }
+
+    const editItem = (item) => {
+        setModalHeader("Edytowanie wpisu");
+        setNew(false);
+        setObj(item);
+        editModal.onOpen();
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.currentTarget));
+        let [method, path] = isNew ? 
+                             ["POST", "dodaj/nowe_zgloszenie"] : 
+                             ["PATCH", ""];
+        console.log(data);
+    }
+
     const renderCell = React.useCallback((repair, columnKey) => {
         const cellValue = repair[columnKey]
 
@@ -60,54 +121,212 @@ export default function Naprawy() {
     }, []);
 
     return (
-        <Table
-            aria-label="Repairs table"
-            sortDescriptor={list.sortDescriptor}
-            onSortChange={list.sort}
-        >
-            <TableHeader>
-                <TableColumn key="naprawaid" allowsSorting>
-                    ID
-                </TableColumn>
-                <TableColumn key="stan" allowsSorting>
-                    Stan
-                </TableColumn>
-                <TableColumn key="dataRozpoczecia" allowsSorting>
-                    Data Rozpoczecia
-                </TableColumn>
-                <TableColumn key="dataZakonczenia" allowsSorting>
-                    Data Zakończenia
-                </TableColumn>
-                <TableColumn key="opis_usterki" allowsSorting>
-                    Opis usterki
-                </TableColumn>
-                <TableColumn key="protokol_naprawy" allowsSorting>
-                    Protokol
-                </TableColumn>
-                {/* <TableColumn key="vin">
-                    VIN
-                </TableColumn>
-                <TableColumn key="mechanikid">
-                    ID mechanika
-                </TableColumn> */}
-            </TableHeader>
-            <TableBody 
-                emptyContent={"Brak wpisów do wyświetlenia."}
-                isLoading={isLoading}
-                items={list.items}
-                loadingContent={<Spinner label="Ładowanie..." />}
-            >
-                {(item) => (
-                    <TableRow 
-                        key={item.naprawaid}
-                        as={Link}
-                        href={`/dashboard/naprawy/${item.naprawaid}`}
-                        className="hover:bg-gray-100 cursor-pointer"
+        <>
+            <div className="flex justify-between items-end mx-4">
+                <Input 
+                    isClearable
+                    className="w-full sm:max-w-[33%]"
+                    placeholder="Wyszukaj..."
+                    startContent={<Image 
+                        src="/search-svgrepo-com.svg"
+                        width={800}
+                        height={800}
+                        alt="Search icon"
+                        style={{
+                            width: "1rem",
+                        }}
+                    />}
+                    value={filterValue}
+                    onClear={() => onClear()}
+                    onValueChange={onSearchChange}
+                />
+                <Button variant="flat" color="success" onPress={() => addItem()}>
+                    Nowe zgłoszenie
+                </Button>
+            </div>
+            <div className="p-4">
+                <Table
+                    removeWrapper
+                    aria-label="Repairs table"
+                    sortDescriptor={list.sortDescriptor}
+                    onSortChange={list.sort}
                     >
-                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
+                    <TableHeader>
+                        <TableColumn key="naprawaid" allowsSorting>
+                            ID
+                        </TableColumn>
+                        <TableColumn key="stan" allowsSorting>
+                            Stan
+                        </TableColumn>
+                        <TableColumn key="dataRozpoczecia" allowsSorting>
+                            Data Rozpoczecia
+                        </TableColumn>
+                        <TableColumn key="dataZakonczenia" allowsSorting>
+                            Data Zakończenia
+                        </TableColumn>
+                        <TableColumn key="opis_usterki" allowsSorting>
+                            Opis usterki
+                        </TableColumn>
+                        <TableColumn key="protokol_naprawy" allowsSorting>
+                            Protokol
+                        </TableColumn>
+                        {/* <TableColumn key="vin">
+                            VIN
+                            </TableColumn>
+                            <TableColumn key="mechanikid">
+                            ID mechanika
+                            </TableColumn> */}
+                    </TableHeader>
+                    <TableBody 
+                        emptyContent={"Brak wpisów do wyświetlenia."}
+                        isLoading={isLoading}
+                        items={items}
+                        loadingContent={<Spinner label="Ładowanie..." />}
+                        >
+                        {(item) => (
+                            <TableRow 
+                            key={item.naprawaid}
+                            as={Button}
+                            onClick={() => editItem(item)}
+                            className="hover:bg-gray-100 cursor-pointer"
+                            >
+                                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            { obj && (
+                <Modal isOpen={editModal.isOpen} onOpenChange={editModal.onOpenChange} scrollBehavior="outside" size="5xl">
+                    <ModalContent>
+                        {(onClose) => (
+                            <Form onSubmit={onSubmit} autoComplete="off">
+                                <ModalHeader className="flex flex-col gap-1">{modalHeader}</ModalHeader>
+                                <ModalBody className="w-full">
+                                    <Input 
+                                        className="w-24"
+                                        isReadOnly
+                                        label="ID"
+                                        name="naprawaid"
+                                        defaultValue={obj["naprawaid"]}
+                                        labelPlacement="outside-left"
+                                    />
+                                    <Input
+                                        label="Stan"
+                                        name="stan"
+                                        defaultValue={obj["stan"]}
+                                        labelPlacement="outside-left"
+                                    />
+                                    <Input
+                                        isReadOnly
+                                        label="Data Rozpoczęcia"
+                                        name="dataRozpoczecia"
+                                        defaultValue={obj["dataRozpoczecia"]}
+                                        labelPlacement="outside-left"
+                                    />
+                                    <Input
+                                        label="Data Zakończenia"
+                                        name="dataZakonczenia"
+                                        defaultValue={obj["dataZakonczenia"]}
+                                        labelPlacement="outside-left"
+                                    />
+                                    <Textarea 
+                                        className="w-full"
+                                        label="Opis usterki"
+                                        name="opis_usterki"
+                                        defaultValue={obj["opis_usterki"]}
+                                        labelPlacement="outside"
+                                    />
+                                    <Textarea 
+                                        className="w-full"
+                                        label="Protokół naprawy"
+                                        name="protokol_naprawy"
+                                        defaultValue={obj["protokol_naprawy"]}
+                                        labelPlacement="outside"
+                                    />
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Zamknij
+                                    </Button>
+                                    <Button color="primary" type="submit" onPress={onClose}>
+                                        Zapisz
+                                    </Button>
+                                </ModalFooter>
+                            </Form>
+                        )}
+                    </ModalContent>
+                </Modal>
+            )}
+            
+            <Modal isOpen={newModal.isOpen} onOpenChange={newModal.onOpenChange} scrollBehavior="outside">
+                <ModalContent>
+                    {(onClose) => (
+                        <Form onSubmit={onSubmit} autoComplete="off">
+                            <ModalHeader className="flex flex-col gap-1">{modalHeader}</ModalHeader>
+                            <ModalBody className="w-full">
+                                <h1>Klient</h1>
+                                <Input
+                                    label="Imię"
+                                    name="imie"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Nazwisko"
+                                    name="nazwisko"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Telefon"
+                                    name="telefon"
+                                    type="tel"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Email"
+                                    name="email"
+                                    type="email"
+                                    labelPlacement="outside-left"
+                                />
+                                <h1>Pojazd</h1>
+                                <Input 
+                                    label="Rejestracja"
+                                    name="rejestracja"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Marka"
+                                    name="marka"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Model"
+                                    name="model"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="Rocznik"
+                                    name="rocznik"
+                                    labelPlacement="outside-left"
+                                />
+                                <Input 
+                                    label="VIN"
+                                    name="vin"
+                                    labelPlacement="outside-left"
+                                />
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Zamknij
+                                </Button>
+                                <Button color="primary" type="submit" onPress={onClose}>
+                                    Zapisz
+                                </Button>
+                            </ModalFooter>
+                        </Form>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
